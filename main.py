505f7446aff0691b143ec02902192a47d72ff5b7
illmonet <112196463+illmonet@users.noreply.github.com>
2025-05-07 13:55:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Securely store Entry data
-valid_entries = {
+entries = {
     "106": "Sandhyamol PK",
     "107": "keerthana jobin",
-    "109": "Saranya Manoj Plappally",
+    "109": "Saranya Manoj",
     "119": "Leenu Binoy",
     "120": "Shalu Rajendraprasad",
-    "121": "Renju Valleetta",
-    "123": "Ambly Jomon Wembly",
+    "121": "Renju Shalu",
+    "123": "Ambly Jomon",
     "133": "Krishnanand MR",
-    "134": "Linimol Ligi Koottickal",
+    "134": "Linimol Igi",
     "137": "Sathi Ravi",
-    "144": "vineetha ps Thalunkal",
+    "144": "vineetha ps",
     "159": "Leenu Akhil",
-    "165": "Sandrakrishna S Koottickal",
-    "169": "Anugraha Manesh Poovanchi",
-    "176": "Ajeesh T V  Chathenplappally",
-    "188": "Arya Asish Yenthayar",
-    "194": "SNEHA LALRAJ Kuttiplangadu",
-    "196": "ARYA SURESH KOKKAYAR",
+    "165": "Sandrakrishna s",
+    "169": "Anugraha Manesh",
+    "176": "Ajeesh T V",
+    "188": "Arya Asish",
+    "194": "SNEHA LALRAJ",
+    "196": "ARYA SURESH",
     "197": "Akhila Vijayan",
     "199": "Sreedevi Thalumkal"
 }
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        entry_no = request.form.get('entry')
+        name = entries.get(entry_no)
+        if name:
+            return render_template('result.html', success=True, name=name, entry_no=entry_no)
+        else:
+            return render_template('result.html', success=False)
     return render_template('index.html')
-
-@app.route('/result', methods=['POST'])
-def result():
-    entry_no = request.form.get('entry')
-    name = valid_entries.get(entry_no)
-    if name:
-        first_two_words = ' '.join(name.split()[:2])
-        return render_template('result.html', success=True, name=first_two_words, entry_no=entry_no)
-    else:
-        return render_template('result.html', success=False)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
