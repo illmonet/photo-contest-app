bd2571f3b2a62f77d17cf1ea555bbacb0e95c104
illmonet <112196463+illmonet@users.noreply.github.com>
2025-05-07 13:07:42 +0530

    Create effect.js

---
+++ static/effect.js
@@ -0,0 +1,5 @@
+function showForm() {
+  document.getElementById("entry-form").classList.remove("hidden");
+  document.querySelector(".center-button").style.display = "none";
+  document.querySelector(".background").style.opacity = "0.15";
+}
